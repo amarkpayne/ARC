71de3bd2393bba2494e095847c86715cc7524f5f
Mark Payne <ampayne@mit.edu>
2020-02-24 17:38:44 -0500

    Add wb97m-v to qchem list

--- arc/settings.py
+++ arc/settings.py
@@ -87,7 +87,7 @@ supported_ess = ['gaussian', 'molpro', 'orca', 'qchem', 'terachem']  # use lower
 levels_ess = {
     'gaussian': ['apfd', 'b3lyp', 'm062x'],
     'molpro': ['ccsd', 'cisd', 'vpz'],
-    'qchem': ['m06-2x'],
+    'qchem': ['m06-2x', 'wb97m-v'],
     'orca': ['dlpno'],
     'terachem': ['pbe'],
 }
